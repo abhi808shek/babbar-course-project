32f439cc138c89fc53099ee3901f1f75faca6535
abhi808shek <abhi808gupta@gmail.com>
2023-04-09 23:08:01 +0530

    first commit

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1 className='font-bold bg-[#0B2447] text-3xl text-center text-white py-2'>Top Courses</h1>
+    </div>
+  )
+}
+
+export default Navbar;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,jsx}",
+  "./index.html",
+  "./Components/**/*.{html,js,jsx}"],
+
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
